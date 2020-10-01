66583a5480aa80351834096b256d9f86ab211e48
Samuel Girardin <samuelgirardin@pm.me>
2020-10-01 15:13:38 +0200

    bump to 0.2.0

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version="0.1.0",
+    version="0.2.0",
     packages=[
         "masonite.inertia",
         "masonite.inertia.providers",
@@ -73,7 +73,7 @@ setup(
     # $ pip install your-package[dev,test]
     extras_require={
         "test": ["coverage", "pytest", "pytest-cov", "coveralls"],
-        "dev": ["black", "flake8", "twine>=1.5.0", "wheel"]
+        "dev": ["black", "flake8", "twine>=1.5.0", "wheel"],
     },
     # If there are data files included in your packages that need to be
     # installed, specify them here.  If using Python 2.6 or less, then these
