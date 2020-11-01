3c35a9c11abdda1913c6fe11d1ba339a2f07d970
Samuel Girardin <samuelgirardin@pm.me>
2020-11-01 20:06:43 +0100

    feat: add request arg to lazy loaded props
    fix #9

--- app/http/controllers/TestController.py
+++ app/http/controllers/TestController.py
@@ -1,6 +1,4 @@
 """A TestController Module."""
-
-from masonite.request import Request
 from masonite.controllers import Controller
 from masonite.inertia import InertiaResponse
 
@@ -9,4 +7,16 @@ class TestController(Controller):
     """TestController Controller Class."""
 
     def custom_id(self, view: InertiaResponse):
-        return view.render("Index", {}, "test_custom_id")
+        return view.render("Index", custom_root_view="test_custom_id")
+
+    def lazy_view(self, view: InertiaResponse):
+        def get_count():
+            return 2
+
+        return view.render("Index", {"count": get_count})
+
+    def lazy_view_with_request(self, view: InertiaResponse):
+        def is_authenticated(request):
+            return request.user()
+
+        return view.render("Index", {"is_authenticated": is_authenticated})
