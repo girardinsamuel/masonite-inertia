daf25025be69a8f9e0bbb35527a38ca9625b2644
Samuel Girardin <samuelgirardin@pm.me>
2021-01-02 12:01:53 +0100

    prepare new 3.0 release

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version="0.4.1",
+    version="3.0.0a1",
     packages=[
         "masonite.inertia",
         "masonite.inertia.providers",
