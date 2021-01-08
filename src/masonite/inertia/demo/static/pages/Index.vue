74e3aa16abc4cfd1edbb01560008666cdc90fadf
Samuel Girardin <samuelgirardin@pm.me>
2021-01-09 00:43:11 +0100

    add demo and demo command

---
+++ src/masonite/inertia/demo/static/pages/Index.vue
@@ -0,0 +1,27 @@
+<template>
+  <img alt="Vue logo" src="https://v3.vuejs.org/logo.png" />
+  <h1>{{ msg }}</h1>
+  <inertia-link href="/inertia-hello">Open link</inertia-link>
+</template>
+
+<script>
+export default {
+  name: "App",
+  data() {
+    return {
+      msg: "Hello Masonite, Vue 3.0 and Inertia !"
+    }
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
