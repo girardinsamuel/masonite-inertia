63c5334aa833e79812e32219fa2677089793df39
Samuel Girardin <samuelgirardin@pm.me>
2021-05-01 14:40:25 +0200

    fix packaging

--- setup.py
+++ setup.py
@@ -12,7 +12,6 @@ setup(
     packages=[
         "masonite.inertia",
         "masonite.inertia.config",
-        "masonite.inertia.demo",
         "masonite.inertia.providers",
         "masonite.inertia.middleware",
         "masonite.inertia.commands",
