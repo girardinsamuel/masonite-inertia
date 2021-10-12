73571fc6c36c3a9989f7cf1df5aa25e7c6918d36
Samuel Girardin <samuelgirardin@pm.me>
2021-10-12 19:09:36 +0200

    fix kernel import

--- wsgi.py
+++ wsgi.py
@@ -1,8 +1,8 @@
 import os
 
 from masonite.foundation import Application, Kernel
-from tests.integrations.app.Kernel import Kernel as AppKernel
 from tests.integrations.config.providers import PROVIDERS
+from tests.integrations.Kernel import Kernel as AppKernel
 
 application = Application(os.getcwd())
 
