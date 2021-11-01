b14789c2d641d6a8304cea0c198d172c74dc5a8d
Samuel Girardin <samuelgirardin@pm.me>
2021-11-01 11:56:34 +0100

    remove old install command

--- src/masonite/inertia/providers/InertiaProvider.py
+++ src/masonite/inertia/providers/InertiaProvider.py
@@ -1,13 +1,10 @@
 """A InertiaProvider Service Provider."""
-# import os
 from masonite.configuration import config
 from masonite.packages.providers import PackageProvider
 
 from ..core.Inertia import Inertia
 from ..helpers import inertia
 from ..testing import InertiaTestingResponse
-
-# package_root = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
 class InertiaProvider(PackageProvider):
@@ -22,15 +19,6 @@ class InertiaProvider(PackageProvider):
         inertia = Inertia(self.application, config("inertia"))
         self.application.bind("inertia", inertia)
         self.application.make("tests.response").add(InertiaTestingResponse)
-
-    # def register(self):
-    #     # Config.merge_with("inertia", os.path.join(package_root, "config/inertia.py"))
-    #     Config.merge_with("inertia", "masonite.inertia.config.inertia")
-    #     inertia = Inertia(self.application, config("inertia"))
-    #     self.application.bind("inertia", inertia)
-
-    #     self.application.make("commands").add(InstallCommand(), DemoCommand())
-    #     self.application.make("tests.response").add(InertiaTestingResponse)
 
     def boot(self):
         self.application.make("view").share({"inertia": inertia})
